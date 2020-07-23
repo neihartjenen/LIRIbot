e6d81143279e1081f696984c03ee52fc7525bf71
jenenneihart <neihart.jenen@gmail.com>
2020-07-23 15:51:38 -0700

    created getspotify function, bot is running but not grabbing data

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
-// console.log('this is loaded');
+console.log('this is loaded');
 
 // Spotify API
 
 exports.spotify = {
-  id: process.env.e4a598809f9f4441a7635828c8d846f3,
-  secret: process.env.cba5b7f6c1f44555b3ef934dfa468298
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 
--- liri.js
+++ liri.js
@@ -11,13 +11,14 @@ var request = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
 
 
-function liriBot (request, userInput) {
+function liriBot(request, userInput) {
+    
     switch (request) {
         case "concerts-this":
             getBandsInTown(userInput);
             break;
 
-        case "spotify-song":
+        case "spotify-this":
             getSpotify(userInput);
             break;
 
@@ -25,8 +26,48 @@ function liriBot (request, userInput) {
             getOMDB(userInput);
             break;
 
-        case "follow-command":
+        case "do-whatever":
             getRandom();
         break;
     };
 };
+
+function getSpotify(songName) {
+
+    var spotify = new Spotify(keys.spotify);
+
+        if(!songName) {
+            songName = "Error"
+        };
+
+    spotify.search({type: 'track', query: songName}, function (err, data) {
+        if(err) {
+        return console.log('error occurred: ' + err);
+    };
+    
+    // Line break
+    console.log("\r\n" + "===============================" + "\r\n");
+    // Returns Artists
+    console.log("Artists Name: " + data.tracks.items[0].album.artists[0].name + "\r\n");
+    // Returns Songs Name
+    console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
+    // Returns preview link of song 
+    console.log("Song Preview Link: " + data.tracks.items[0].href + "\r\n");
+    // Returns the name of the Album that the song is from
+    console.log("Album: " + data.tracks.items[0].album.name + "\r\n");
+
+   //  Puts results into txt file 
+    var logSong = "\r\n" + "===== Begin spotify-this log =====" + "\r\n" + "\nArtist: " + data.tracks.items[0].album.artists[0].name + "\r\n" + "\nSong Name: " + data.tracks.items[0].name + "\r\n" +  "\nSong Preview Link: " + data.tracks.items[0].href + "\r\n" +  "\nAlbum: " + data.tracks.items[0].album.name + "\r\n" + "\n===== End spotify-this =====" + "\r\n";
+
+    fs.appendFile("log.txt", logSong, function(err) {
+      if (err) throw err;
+    });
+ });
+};
+
+liriBot(request, userInput)
+
+// function getBandsInTown(artists) {
+
+//     var artists = userInput;
+// }
